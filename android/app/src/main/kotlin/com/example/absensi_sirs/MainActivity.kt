8ee797755dcb65f64197306b270f1f07254199a5
vgdef <valensyagdef@icloud.com>
2021-04-20 09:47:00 +0900

    new

---
+++ android/app/src/main/kotlin/com/example/absensi_sirs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.absensi_sirs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:absensi_sirs/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:absensi_sirs/screen/home/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'SIRS Leimena',
+      theme: ThemeData(
+        scaffoldBackgroundColor: kBackgroundColor,
+        primaryColor: kPrimaryColor,
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
