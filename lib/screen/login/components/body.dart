c7a16a1381243600f730bdf7e23c133131c90945
vgdef <valensyagdef@icloud.com>
2021-04-20 15:21:45 +0900

    new

---
+++ lib/screen/login/components/body.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      children: <Widget>[
+        Text('Hellow')
+      ],
+    );
+        }
+}
---
+++ lib/screen/login/login_screen.dart
@@ -0,0 +1,15 @@
+import 'package:absensi_sirs/screen/home/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:absensi_sirs/screen/login/components/body.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:absensi_sirs/screen/home/home_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
